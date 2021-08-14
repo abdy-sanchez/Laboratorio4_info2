9f683c70ac9e81b09626f83426ff5bff0ff85ee9
abdy-sanchez <abdy.sanchez@udea.edu.co>
2021-08-13 20:46:26 -0500

    Lab 4 Terminado :)))

---
+++ Laboratorio_4/programa.h
@@ -0,0 +1,11 @@
+#ifndef PROGRAMA_H
+#define PROGRAMA_H
+
+#include "red.h"
+
+        //Prototipos de las funciones
+
+void programa_principal();
+
+
+#endif // PROGRAMA_H
---
+++ Laboratorio_4/red.h
@@ -0,0 +1,52 @@
+#ifndef RED_H
+#define RED_H
+
+#include "enrutador.h"
+
+        //Clase para modelar una Red de enrutadores
+
+
+class Red{
+
+
+
+    private:
+
+        map <char,Enrutador> red_enrutadores ;      //Contenedor de los enrutadores
+
+        map <char,Enrutador>::iterator iterador_red ;       //Iterador de la Red
+
+        map <char,Enrutador>::iterator iterador_red2 ;       //Iterador de la Red 2
+
+    public:
+
+            //Metodos de la clase Red
+
+       void almacenar_enrutador( Enrutador objeto_enrutador ) ;
+
+       void tamano_red() ;
+
+       void Crear_Arista() ;
+
+       void Lista_adyacencia() ;
+
+       void Eliminar_Enrutador() ;
+
+       void Eliminar_Arista() ;
+
+       void Modificar_peso() ;
+
+       void generacion_automatica() ;
+
+       void Envio_paquete() ;
+
+       void red_aleatoria() ;
+
+
+};
+
+
+bool comparacion( pair<char, unsigned int> a, pair<char, unsigned int>b );      //Funcion complementaria para
+                                                                                //el metodo Envio_paquete()
+
+#endif // RED_H
